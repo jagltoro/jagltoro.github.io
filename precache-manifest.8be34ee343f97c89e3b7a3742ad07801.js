7d8cadd5413338d092916d34c5b0b08ee9ce9986
Jesus Gonzalez <jagltoro@gmail.com>
2020-05-09 18:27:05 -0300

    New website

---
+++ precache-manifest.8be34ee343f97c89e3b7a3742ad07801.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff65ccc76c9ed3252cec2009ae218b9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de9ac739eab097d31e24",
+    "url": "/static/css/2.84ac1de2.chunk.css"
+  },
+  {
+    "revision": "1d3e07545db0a02b74c1",
+    "url": "/static/css/4.190caba9.chunk.css"
+  },
+  {
+    "revision": "b0ae06876fac00a4c382",
+    "url": "/static/css/main.8fc40a4f.chunk.css"
+  },
+  {
+    "revision": "de9ac739eab097d31e24",
+    "url": "/static/js/2.c481c2c0.chunk.js"
+  },
+  {
+    "revision": "f3ad5a8a13cbcfa3c2ce",
+    "url": "/static/js/3.c2b0c479.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/3.c2b0c479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3e07545db0a02b74c1",
+    "url": "/static/js/4.36c1ce6e.chunk.js"
+  },
+  {
+    "revision": "d8080348ff2064724b36f2ca2f82999a",
+    "url": "/static/js/4.36c1ce6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ae06876fac00a4c382",
+    "url": "/static/js/main.4914fa8d.chunk.js"
+  },
+  {
+    "revision": "8696f398cf9cc4f865f1",
+    "url": "/static/js/runtime-main.240f0627.js"
+  },
+  {
+    "revision": "999fa2c464e7abffc68c35bf8470cc5c",
+    "url": "/static/media/alana.999fa2c4.webp"
+  },
+  {
+    "revision": "e209da9c166aaa7771a72dbe46b9afa3",
+    "url": "/static/media/cgpack.e209da9c.webp"
+  },
+  {
+    "revision": "701f79654f81fac2384bf8c59471471b",
+    "url": "/static/media/desk.701f7965.svg"
+  },
+  {
+    "revision": "bdd530ed71942c1ce8c4cb5fa14d8597",
+    "url": "/static/media/estrella.bdd530ed.webp"
+  },
+  {
+    "revision": "c2a806bf0616444b5eefc2e23632e155",
+    "url": "/static/media/idleGame.c2a806bf.webp"
+  },
+  {
+    "revision": "40e40d70dd50c472fb41378ac7912ee2",
+    "url": "/static/media/logo.40e40d70.svg"
+  },
+  {
+    "revision": "586febe7b8c227c8842f51749a52d5bf",
+    "url": "/static/media/movies.586febe7.webp"
+  },
+  {
+    "revision": "f974e561d5f1451f5e44891347e8b890",
+    "url": "/static/media/mrpresta.f974e561.webp"
+  },
+  {
+    "revision": "04f501c1d060f0dc1fbd6f3a52438537",
+    "url": "/static/media/natural.04f501c1.webp"
+  },
+  {
+    "revision": "b125b47ed7745ea9071a3e85184beb16",
+    "url": "/static/media/nps-dev.b125b47e.webp"
+  },
+  {
+    "revision": "09b0af7b768ee9c1bc3a466686c92500",
+    "url": "/static/media/performer.09b0af7b.webp"
+  },
+  {
+    "revision": "82316f271492f512de9d417acbe44e44",
+    "url": "/static/media/phone-view.82316f27.svg"
+  },
+  {
+    "revision": "f10b3f064f190258b593ea8f426dd2b7",
+    "url": "/static/media/profile.f10b3f06.webp"
+  },
+  {
+    "revision": "6049d04ebae54b5ac1837a96d8b18b6c",
+    "url": "/static/media/reminder.6049d04e.webp"
+  },
+  {
+    "revision": "bd7d3fbeb615beb4c2cf297b0b1dae6e",
+    "url": "/static/media/retention.bd7d3fbe.webp"
+  }
+]);
